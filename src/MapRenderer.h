80270ddc0e6ab97f9d3a32b20a8bf88c61b3c892
Justin Jacobs <jajdorkster@gmail.com>
2025-09-15 13:52:59 -0400

    Use hero_pos_id for overriding map hero_pos

--- src/MapRenderer.h
+++ src/MapRenderer.h
@@ -136,6 +136,7 @@ public:
 	// teleport handling
 	bool teleportation;
 	FPoint teleport_destination;
+	int teleport_destination_id;
 	std::string teleport_mapname;
 	std::string respawn_map;
 	FPoint respawn_point;
